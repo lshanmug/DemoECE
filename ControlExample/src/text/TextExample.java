3f3a68553d933a9437452a0ba91f3123875107a9
Lakshmi Shanmugam <lshanmug@in.ibm.com>
2019-10-12 15:15:20 +0200

    Added text example

---
+++ ControlExample/src/text/TextExample.java
@@ -0,0 +1,34 @@
+package text;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
+
+public class TextExample {
+public static void main(String[] args) {
+	Display display = new Display();
+	Shell shell = new Shell(display);
+	shell.setLayout(new GridLayout(1, false));
+
+	Group group = new Group(shell, SWT.NONE);
+	group.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false));
+	group.setLayout(new GridLayout(1, false));
+	group.setText("Text group");
+	
+	Text text = new Text(group, SWT.SINGLE | SWT.LEAD | SWT.BORDER);
+	text.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
+	text.setText("Single Text field");
+	
+	shell.pack();
+	shell.open();
+	while (!shell.isDisposed()) {
+		if (!display.readAndDispatch())
+			display.sleep();
+	}
+	display.dispose();
+}
+}
